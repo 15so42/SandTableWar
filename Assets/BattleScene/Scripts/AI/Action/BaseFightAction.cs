0512f60dbd7451e17852a96c7bb2829e6259cc37
Sa <1293014013@qq.com>
2020-12-13 21:44:47 +0800

    1.基础状态加增加战斗时点击地面强行移动逻辑 2.战斗状态任务转向瞄准敌人，子弹向量计算

--- Assets/BattleScene/Scripts/AI/Action/BaseFightAction.cs
+++ Assets/BattleScene/Scripts/AI/Action/BaseFightAction.cs
@@ -1,12 +1,18 @@
 ﻿
+    using UnityEngine;
+
     public class BaseFightAction : StateAction
     {
         //用于只执行一次
         public bool isStopped = false;
+        private  Vector3 rotateVel;
         public override void Act(StateController controller)
         {
             //停止移动,但是目标地点不变，当战斗结束后将isStopped设置为false继续像目标位置前进
             controller.navMeshAgent.isStopped = true;
+            Transform ownerTransform = controller.owner.transform;
+            Vector3 dir = controller.enemy.transform.position - ownerTransform.position;
+            ownerTransform.forward = Vector3.SmoothDamp(ownerTransform.forward, dir, ref rotateVel, 0.3f);
             controller.owner.weapon.WeaponUpdate();
         }
     }
--- Assets/BattleScene/Scripts/AI/StateController.cs
+++ Assets/BattleScene/Scripts/AI/StateController.cs
@@ -82,16 +82,18 @@ public class StateController
         //                  F：fight
         //    F:idle
         fightState.AddAction(new BaseFightAction());
+        //转换条件按添加顺序执行，满足一个转换条件后就转换，因此此处是在战斗中先判断目标路径点是否发生了变化，是则强行移动
+        fightState.AddTransition(new Transition()
+        {
+            decisions = new List<Decision>{new HasTargetPosDecision()},
+            falseState = fightState,
+            trueState = moveIgnoreEnemyState
+        });
+        //如果路径点没有发生变化，则判断敌人是否全被消灭,全被消灭后转换为MoveState朝之前的目标点移动
         fightState.AddTransition(new Transition()
         {
             decisions = new List<Decision>{new FindEnemyDecision()},
-            falseState = idleState,
-            trueState = fightState
-        });
-        fightState.AddTransition(new Transition()
-        {
-            decisions =  new List<Decision>{new ReachTargetPosDecision()},
-            falseState = moveIgnoreEnemyState,
+            falseState = moveState,
             trueState = fightState
         });
 
