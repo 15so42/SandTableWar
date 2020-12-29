bc83a18fc94ebbd1b6946c8878adc68483db951e
Sa <1293014013@qq.com>
2020-12-30 02:15:48 +0800

    建筑UI基本生成完成，还差实时跟随以及图片、生成数量、可点击状态等

---
+++ Assets/BattleBuildingMenuDialog.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingMenuDialogContext : DialogContext
+{
+   public BaseBattleBuilding targetUnitBase;
+   public string[] menus;
+   public int r = 120;
+
+   public BuildingMenuDialogContext(BaseBattleBuilding targetUnitBase,string[] menus)
+   {
+      this.targetUnitBase = targetUnitBase;
+      this.menus = menus;
+   }
+}
+public class BattleBuildingMenuDialog : Dialog<BuildingMenuDialogContext>
+{
+   //此类为菜单dialog，内含多个按钮
+   public Transform btnParent;
+   public const string pathPrefix = "Prefab/UI/Menu/";
+   public static Dialog ShowDialog(BaseBattleBuilding targetUnitBase, string[] menus)
+   {
+      var dialog = GetShowingDialog(nameof(BattleBuildingMenuDialog)) as BattleBuildingMenuDialog;
+      if (dialog != null)
+      {
+         return null;
+      }
+
+      return DialogUtil.ShowDialogWithContext(nameof(BattleBuildingMenuDialog), new BuildingMenuDialogContext(targetUnitBase,menus));
+   }
+
+   public override void Show()
+   {
+      base.Show();
+      string[] menus = dialogContext.menus;
+      for (int i=0;i<menus.Length;i++)
+      {
+         //解析命令
+         string[] splitedStrings = menus[i].Split('_');
+         string command = splitedStrings[0].ToLower();
+         string param0="";
+         string param1="";
+         if (splitedStrings.Length > 1)
+         {
+            param0 = splitedStrings[1].ToLower();
+         }
+
+         if (splitedStrings.Length > 2)
+         {
+            param1 = splitedStrings[2].ToLower();
+         }
+       
+         switch (command)
+         {
+            case "spawn" : //生成单位
+               InstantiateSpawnBtn(i,int.Parse(param0),float.Parse(param1));
+               break;
+            case "close" ://关闭菜单
+               InstantiateCloseBtn(i);
+               break;
+         }
+      }
+   }
+
+   private void InstantiateSpawnBtn(int index,int spawnId,float priceOff)//折扣，受各种buff、科技、政策影响
+   {
+      GameObject spawnPfb =
+         Resources.Load<GameObject>(pathPrefix + "SpawnButton");
+      GameObject iBtn = Instantiate(spawnPfb,GetBtnPosByIndex(index),Quaternion.identity,btnParent);
+      
+      iBtn.GetComponent<Button>().onClick.AddListener(()=>
+      {
+         dialogContext.targetUnitBase.AddUnitToSpawnStack(spawnId);
+      });
+   }
+   
+   private void InstantiateCloseBtn(int index)
+   {
+      GameObject closePfb =
+         Resources.Load<GameObject>(pathPrefix +"CloseButton");
+      GameObject iBtn = Instantiate(closePfb,GetBtnPosByIndex(index),Quaternion.identity,btnParent);
+      iBtn.GetComponent<Button>().onClick.AddListener(Close);
+   }
+
+   private Vector3 GetBtnPosByIndex(int index)
+   {
+      Vector3 center = Camera.main.WorldToScreenPoint(dialogContext.targetUnitBase.transform.position);
+      int total = dialogContext.menus.Length;
+      int degree = 360 / total;
+      Vector3 pos = center + new Vector3(dialogContext.r * Mathf.Sin(index * degree),
+         dialogContext.r * Mathf.Cos(index * degree), 0);
+      return pos;
+   }
+}
--- Assets/BattleScene/Scripts/BattleBuilding/BaseBattleBuilding.cs
+++ Assets/BattleScene/Scripts/BattleBuilding/BaseBattleBuilding.cs
@@ -5,34 +5,33 @@ using UnityEngine;
 
 public class BaseBattleBuilding : BattleUnitBase
 {
-    //临时存储id和对应的动态加载的名称
-    public Dictionary<int,string> soliderIdTable=new Dictionary<int, string>()
-    {
-        {1,"Solider1"}
-    };
-
     private float timer = 0;
     private float interval = 0;
     
-    public int spawnId=1;//作为建筑一般是产出士兵等活动单位，士兵等活动单位通过id确定
+    //public int spawnId=1;//作为建筑一般是产出士兵等活动单位，士兵等活动单位通过id确定
 
-    private SpawnBattleUnitConfigInfo curSpawnInfo;
+    //private SpawnBattleUnitConfigInfo curSpawnInfo;
 
     public Transform spawnPos;
+
+    public Stack<int> toSpawn=new Stack<int>();
+    [Header("建筑菜单，使用字符串表示对应菜单")]
+    public string[] menuCommands;
+    
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         stateController = null;//建筑单位行为较简单，不使用状态机，需要转换时在对应类中重写
-        ChangeSpawnId(spawnId);
+        //ChangeSpawnId(spawnId);
     }
     
     //更换生成的单位
     private void ChangeSpawnId(int id)
     {
-        spawnId = id;
-        curSpawnInfo = ConfigHelper.Instance.GetSpawnBattleUnitConfigInfoById(id);
-        interval = curSpawnInfo.spawnTime;
+        //spawnId = id;
+        //curSpawnInfo = ConfigHelper.Instance.GetSpawnBattleUnitConfigInfoById(id);
+        //interval = curSpawnInfo.spawnTime;
     }
 
     // Update is called once per frame
@@ -41,27 +40,58 @@ public class BaseBattleBuilding : BattleUnitBase
         base.Update();
         if(photonView.IsMine==false)
             return;
-        //判断资源足够时才计时
-        if (fightingManager.HasEnoughResToSpawnSolider(spawnId))
+        if (toSpawn.Count > 0)
         {
-            timer += Time.deltaTime;
-            if (timer > interval)
+            //判断资源足够时才计时
+            if (fightingManager.HasEnoughResToSpawnSolider(toSpawn.Peek()))
             {
-                timer = 0;
-                SpawnUnit();
+                timer += Time.deltaTime;
+                if (timer > interval)
+                {
+                    timer = 0;
+                    SpawnUnit();
+                }
             }
         }
-        
+      
     }
-
-
+    
     public void SpawnUnit()
     {
-        if (fightingManager.ConsumeResByUnitInfo(curSpawnInfo))
+        SpawnBattleUnitConfigInfo curSpawnInfo=ConfigHelper.Instance.GetSpawnBattleUnitConfigInfoById(toSpawn.Peek());
+        if (fightingManager.ConsumeResByUnitInfo(curSpawnInfo));
         {
             BattleUnitBaseFactory.Instance.SpawnBattleUnitAtPos(curSpawnInfo,spawnPos.position,fightingManager.campId);
         }
-        
+        toSpawn.Pop();
+        //下一个单位
+        if (toSpawn.Count > 0)
+        {
+            interval = ConfigHelper.Instance.GetSpawnBattleUnitConfigInfoById(toSpawn.Peek()).spawnTime;
+        }
+
+        timer = 0;
+
+    }
+
+    public void AddUnitToSpawnStack(int id)
+    {
+        if (toSpawn.Count > 0)
+        {
+            toSpawn.Push(id);
+        }
+        else
+        {
+            toSpawn.Push(id);
+            interval = ConfigHelper.Instance.GetSpawnBattleUnitConfigInfoById(toSpawn.Peek()).spawnTime;
+            timer = 0;
+        }
+    }
+
+    protected override void MouseClickHandle()
+    {
+        base.MouseClickHandle();
+        BattleBuildingMenuDialog.ShowDialog(this,menuCommands);
     }
 }
 
